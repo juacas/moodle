975747ee4d7dd9cf141c2d2df28143b9a6230ce2
Juan Pablo <juan.pablo.de.castro@gmail.com>
2021-01-26 12:10:17 +0000

    Label "trully anon" in complete feedback fixed.

--- mod/feedback/lang/es/feedback.php
+++ mod/feedback/lang/es/feedback.php
@@ -1,4 +1,5 @@
 <?php
 $string['anonymous_voting_undergoing'] = 'Esta votación totalmente anónima no ha terminado. Vuelva más tarde..';
 $string['anonymous_voting_undergoing_help'] = 'Las estadísticas de la votación, para ser verdaderamente anónima, no pueden ser rastreadas durante la votación..';
-$string['trully_anonymous'] = 'Respuestas totalmente incorreladas en la base de datos y listados.';
+$string['trully_anonymous'] = 'Totalmente anónimo.';
+$string['anonymous'] = 'Confidencial (la identidad del usuario no se muestra)'
