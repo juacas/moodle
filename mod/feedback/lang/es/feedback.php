8ec9a5eb215064443d1c6367bd8166a1c6c9f293
Juan Pablo <juan.pablo.de.castro@gmail.com>
2021-01-26 16:47:53 +0000

    message shown while undergoing fixed.

--- mod/feedback/lang/es/feedback.php
+++ mod/feedback/lang/es/feedback.php
@@ -2,4 +2,4 @@
 $string['anonymous_voting_undergoing'] = 'Esta votación totalmente anónima no ha terminado. Vuelva más tarde..';
 $string['anonymous_voting_undergoing_help'] = 'Las estadísticas de la votación, para ser verdaderamente anónima, no pueden ser rastreadas durante la votación..';
 $string['trully_anonymous'] = 'Totalmente anónimo.';
-$string['anonymous'] = 'Confidencial (la identidad del usuario no se muestra)'
+$string['anonymous'] = 'Confidencial (la identidad del usuario no se muestra)';
